0082892fa79cde883a2a274a7e046aceb8a457bc
settler <adnanvahora144@gmail.com>
2021-08-26 17:37:01 +0530

    added definition and structure

---
+++ 2_5_Method_overloading/src/WashingMachine.java
@@ -0,0 +1,12 @@
+/**
+ * @author 20CE155_ADNAN_VAHORA
+ * @Question Develop a Program that illustrate method overloading concept.
+ *
+ * Here I have made washing machine program in which I can virtualy wash clothes.
+ * machine has different functionality like Wash, Rinse and Spin.
+ * and have different modes like speedy, delicate, heavy, normal.
+ * and have warning for low water.
+ */
+public class WashingMachine {
+
+}
