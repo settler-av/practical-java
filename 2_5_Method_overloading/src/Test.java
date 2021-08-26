b47e334ebff97e7758f09f2a01e4cf63f7a841bd
settler <adnanvahora144@gmail.com>
2021-08-26 21:52:02 +0530

    done with major skeleton of Washing machine

--- 2_5_Method_overloading/src/Test.java
+++ 2_5_Method_overloading/src/Test.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        System.out.println("``````````````````````Welcome to Washing machine.````````````````````");
+        waterTank tank = new waterTank();
+        WashingMachine whirlPool = new WashingMachine();
+        Scanner userChoice = new Scanner(System.in);
+        /**
+         * this is machine menu
+         * here user will enter his choice and will run the machine.
+         */
+        do {
+            System.out.println("1.\tPower\t\t" +
+                    "2.\tProgram \t\t" +
+                    "3.\trestart\t\t");
+            System.out.println("Your choice : ");
+            int choice = userChoice.nextInt();
+            switch (choice) {
+                case 1:
+                    whirlPool.power();
+                case 2:
+                    whirlPool.program();
+                case 3:
+                    whirlPool = new WashingMachine();
+            }
 
+        } while (whirlPool.machinePower);
     }
+
+
 }
--- 2_5_Method_overloading/src/WashingMachine.java
+++ 2_5_Method_overloading/src/WashingMachine.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 /**
  * @author 20CE155_ADNAN_VAHORA
  * @Question Develop a Program that illustrate method overloading concept.
- *
+ * <p>
  * Here I have made washing machine program in which I can virtualy wash clothes.
  * machine has different functionality like Wash, Rinse and Spin.
  * and have different modes like speedy, delicate, heavy, normal.
@@ -17,7 +19,8 @@ public class WashingMachine {
     final double waterLevelHigh = 60;
     final double waterLevelVeryHigh = 70;
     double currentWaterLevel;
-
+    String waterLevel = "Normal";
+    int waterLevelIndicator = 2;
     /**
      * variables for time management.
      * use minutes for time
@@ -43,6 +46,80 @@ public class WashingMachine {
     //water in and out Rate use liter/min for all rate.
     final double waterInletRate = 7;
     final double waterDrainRate = 15;
+    //machine
+    boolean machinePower = false;
 
+    void power() {
+        machinePower = !(machinePower);
+    }
+
+    /**
+     * @// TODO: 8/26/2021 make programming in machine
+     */
+    void program() {
+        Scanner userChoice = new Scanner(System.in);
+        System.out.println("select wash Mode : \n1.\tNormal\n" +
+                "2.\tSpeedy\n" +
+                "3.\tDelicate\n" +
+                "4.\theavy\n");
+        System.out.println("Your choice : ");
+        int washMode = userChoice.nextInt();
+
+        System.out.println("select Sub-wash Mode : \n1.\tWash-Rinse-Spin\n" +
+                "2.\tCustom\n");
+        System.out.println("Your choice : ");
+        int subWashMode = userChoice.nextInt();
+
+
+        System.out.println("Current water level is " + waterLevel);
+        System.out.println("Do you want to change it? (Y/N) : ");
+        String waterLevelChange = userChoice.nextLine();
+
+        if (waterLevelChange.equalsIgnoreCase("y")) {
+            System.out.println("1.\tLow \n" +
+                    "2.\tNormal\n" +
+                    "3.\tHigh\n" +
+                    "4.\tVery High\n" +
+                    "Your choice : \n");
+        }
+        waterLevelIndicator = userChoice.nextInt();
+
+        /**
+         * @// TODO: 8/26/2021 for choice 1 - start normal wash, choice 2 - again make menu.
+         */
+        if (subWashMode == 1) {
+            machineRun(washMode, waterLevelIndicator);
+        } else if (subWashMode == 2) {
+            System.out.println("1.\tWash-Rinse\n" +
+                    "2.\tRinse-Spin\n" +
+                    "3.\tWash only\n" +
+                    "4.\tSpin only\n" +
+                    "Your choice : \n");
+            int subWashModeChoice = userChoice.nextInt();
+            boolean[] choiceArr = {true, true, true};
+            if (subWashModeChoice == 1) {
+                choiceArr = new boolean[]{true, true, false};
+            } else if (subWashModeChoice == 2) {
+                choiceArr = new boolean[]{false, true, true};
+            } else if (subWashModeChoice == 3) {
+                choiceArr = new boolean[]{true, false, false};
+            } else if (subWashModeChoice == 4) {
+                choiceArr = new boolean[]{false, false, true};
+            }
+            machineRun(washMode, waterLevelIndicator, choiceArr[0],choiceArr[1],choiceArr[2]);
+        } else {
+            System.out.println("Enter proper choice.");
+        }
+    }
+
+    void machineRun(int mode, int waterLevel) {
+
+
+    }
+
+
+    void machineRun(int mode, int waterLevel, boolean wash, boolean rinse, boolean spin) {
+
+    }
 
 }
