7394ed3d2270d11c93415c38c598f8450bec0c02
settler <adnanvahora144@gmail.com>
2021-10-06 17:36:13 +0530

    add question to the comment

--- 2_1_Constructor/src/Main/Cylinder.java
+++ 2_1_Constructor/src/Main/Cylinder.java
@@ -1,5 +1,15 @@
 package Main;
-
+/**
+ * @author 20CE155 ADNAN VAHORA
+ * Design a class named Cylinder containing following attributes and behavior.
+ *  One double data field named radius. The default value is 1.
+ *  One double data field named height. The default value is 1.
+ *  A no-argument constructor that creates a default Cylinder.
+ *  A Single argument constructor that creates a Cylinder with the specified radius.
+ *  Two argument constructor that creates a Cylinder with the specified radius and height.
+ *  A method named getArea() that returns area of the Cylinder.
+ *  Create a class TestCylinder and test and display result.
+ */
 public class Cylinder {
     double radius;
     double height;
