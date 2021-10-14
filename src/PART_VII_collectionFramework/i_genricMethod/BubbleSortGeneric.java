fdb2587c3b6fe43446e0030f611004603df7705f
settler-av <20ce155@charusat.edu.in>
2021-10-14 10:14:17 +0530

    Done generic sorting

--- src/PART_VII_collectionFramework/i_genricMethod/BubbleSortGeneric.java
+++ src/PART_VII_collectionFramework/i_genricMethod/BubbleSortGeneric.java
@@ -1,42 +1,47 @@
 package PART_VII_collectionFramework.i_genricMethod;
 
-public class BubbleSortGeneric <T extends Comparable<? super T>>{
-    //will use this to store the array
-    T[] array;
+import java.util.Arrays;
 
-    public BubbleSortGeneric(T[] array) {
-        this.array = array;
-    }
+public class BubbleSortGeneric<T extends Comparable<? super T>> {
+    //will use this to store the array
     //this is the main sorting algorithm
-    private T[] BubbleSort(){
+    public T[] BubbleSort(T[] array) {
         // TODO: 13-10-2021 Write a bubble sort algorithm
+        for (int i = 0; i < array.length ; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].compareTo(array[j]) > 0){
+                    T temp;
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
         return array;
     }
-
-
 }
 
-//class test{
-//    // generic method printArray
-//    // TODO: 12-10-2021 write sort method for this program
-//    public static sort()
-//
-//
-//
-//    public static void main(String[] args) {
-//
-//// Create arrays of Integer, Double and Character
-//        Integer[] intArray = {6, 2, 3, 4, 5};
-//        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
-//        Character[] charArray = {'H', 'E', 'L', 'L', 'O'};
-//
-//        System.out.println("Array integerArray contains:");
-//        printArray(intArray); // pass an Integer array
-//
-//        System.out.println("\nArray doubleArray contains:");
-//        printArray(doubleArray); // pass a Double array
-//
-//        System.out.println("\nArray characterArray contains:");
-//        printArray(charArray); // pass a Character array
-//    }
-//}
+class test {
+    public static void main(String[] args) {
+        Integer[] intArray = {6, 2, 3, 4, 5};
+        Double[] doubleArray = {1.1, 5.0, 3.3, 4.4};
+        Character[] charArray = {'H', 'E', 'L', 'L', 'O'};
+
+        System.out.println("Before sorting <Integer> : " + Arrays.toString(intArray));
+        BubbleSortGeneric<Integer> integerObj = new BubbleSortGeneric<Integer>();
+        Integer[] array = integerObj.BubbleSort(intArray);
+        System.out.println("After sorting <Integer> : " + Arrays.toString(array));
+
+        System.out.println("Before sorting <Double> : " + Arrays.toString(doubleArray));
+        BubbleSortGeneric<Double> doubleObj = new BubbleSortGeneric<Double>();
+        Double[] sortedDouble= doubleObj.BubbleSort(doubleArray);
+        System.out.println("After sorting <Double> : " + Arrays.toString(sortedDouble));
+
+        System.out.println("Before sorting <Character> : " + Arrays.toString(charArray));
+        BubbleSortGeneric<Character> characterObj = new BubbleSortGeneric<Character>();
+        Character[] sortedChar = characterObj.BubbleSort(charArray);
+        System.out.println("After sorting <Character> : " + Arrays.toString(sortedChar));
+
+        System.out.println("\nBy 20CE155 ADNAN VAHORA");
+    }
+}
