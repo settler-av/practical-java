77ed0ea14edcb81e6b6466ea590d0b1920558cef
settler-av <20ce155@charusat.edu.in>
2021-10-17 08:35:37 +0530

    Done with multi-catch block

--- src/PART_IV_exception_handling/III_finally/ExceptionWithFinally.java
+++ src/PART_IV_exception_handling/III_finally/ExceptionWithFinally.java
@@ -1,6 +1,32 @@
 package PART_IV_exception_handling.III_finally;
 
-// TODO: 12-10-2021 use of finally  
+import static java.lang.System.exit;
+
 public class ExceptionWithFinally {
+    public static void main(String[] args) {
+        int[] array = {0, 1, 2, 3,0, 0, 5};
+        int exceptionCounter = 0;
+        while(true){
+            try{
+                int indexNum = (int) (Math.random()*10);
+                int indexDen = (int) (Math.random()*10);
+                System.out.println("index of :: Numerator : "+ indexNum + " Denominator : " + indexDen);
+                int numerator = array[indexNum];
+                int denominator = array[indexDen];
+                System.out.println("\tNumerator/Denominator = "+ numerator + "/" + denominator );
+                int result = numerator/denominator;
+
+            }catch (ArrayIndexOutOfBoundsException | ArithmeticException e){
+                exceptionCounter++;
+                System.out.println(e);
+            } finally {
+                if(exceptionCounter > 10){
+                    System.out.println("By 20CE155 ADnAN Vahora\n");
+                    exit(0);
+                }
+                System.out.println("Exception counter = "+exceptionCounter + "\n");
+            }
+        }
+    }
 
 }
