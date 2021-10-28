ac49f4b5c29bb13451a2b8ee38297d6dbc4b508c
settler-av <20ce155@charusat.edu.in>
2021-10-28 20:05:39 +0530

    write program for buffer reader and buffer writer

---
+++ src/PART_V_fileHnadling/V_buffer/Question.md
@@ -0,0 +1,8 @@
+##Only even
+
+---------
+Write a program to enter any 15 numbers from the user and store only even numbers in a file
+named “Even.txt”. And display the contents of this file on the console.
+
+###Hint
+(BufferedReader / BufferedWriter)
---
+++ src/PART_V_fileHnadling/V_buffer/onlyEven.java
@@ -0,0 +1,48 @@
+package PART_V_fileHnadling.V_buffer;
+
+import java.io.*;
+
+/**
+ * @author Adnan Vahora
+ */
+public class onlyEven {
+    public static File destination = new File("src/PART_V_fileHnadling/V_buffer/Even.txt");
+    static int tempInput;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Enter number " + i + ": ");
+            getInput();
+            writeToFile();
+        }
+        System.out.println("Data of EVEN.txt");
+        printFile();
+    }
+
+     static void getInput() {
+//        Scanner sc = new Scanner(System.in);
+//        tempInput = sc.nextInt();
+        tempInput = (int)(Math.random()*10);
+        System.out.println(tempInput);
+    }
+
+    static void writeToFile() {
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(destination,true))) {
+            if (tempInput % 2 == 0) {
+                out.write(tempInput);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static void printFile(){
+        try (BufferedReader in = new BufferedReader(new FileReader(destination))){
+            int temp;
+            while((temp = in.read()) != -1 ){
+                System.out.print(temp + " ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
